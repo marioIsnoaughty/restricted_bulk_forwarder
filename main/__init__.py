072e3cee2b4231cb029790002aba13fac4bb219f
DRAGO <88723798+Drago-Owner@users.noreply.github.com>
2023-03-08 00:27:21 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -19,7 +19,6 @@ API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None)
 SUDO_USERS = []
 if len(AUTH) != 0:
